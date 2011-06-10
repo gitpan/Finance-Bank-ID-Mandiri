5b4d386c63da0e72107f88ad382fa7d0833935b4
Steven Haryanto <stevenharyanto@gmail.com>
2011-06-10 01:02:44 +0000

    Import of SHARYANTO/Finance-Bank-ID-Mandiri-0.16 from CPAN.
    gitpan-cpan-distribution: Finance-Bank-ID-Mandiri
    gitpan-cpan-version:      0.16
    gitpan-cpan-path:         SHARYANTO/Finance-Bank-ID-Mandiri-0.16.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Finance/Bank/ID/Mandiri.pm
+++ lib/Finance/Bank/ID/Mandiri.pm
@@ -1,6 +1,6 @@
 package Finance::Bank::ID::Mandiri;
 BEGIN {
-  $Finance::Bank::ID::Mandiri::VERSION = '0.15';
+  $Finance::Bank::ID::Mandiri::VERSION = '0.16';
 }
 # ABSTRACT: Check your Bank Mandiri accounts from Perl
 
@@ -545,7 +545,7 @@ Finance::Bank::ID::Mandiri - Check your Bank Mandiri accounts from Perl
 
 =head1 VERSION
 
-version 0.15
+version 0.16
 
 =head1 SYNOPSIS
 
